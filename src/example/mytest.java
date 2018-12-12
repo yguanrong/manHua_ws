2efa0f939a4a0980fa07d4d57f2ba44a6d67ebde
yguanrong <Y_guanrong@163.com>
2018-12-12 14:36:11 +0800

    练习更新GitHub

--- src/example/mytest.java
+++ src/example/mytest.java
@@ -3,7 +3,7 @@ package example;
 public class mytest {
 
     public static void main(String[] agr){
-        String url = "https://mhpic.jumanhua.com/comic/D/斗破苍穹拆分版/719话/1.jpg-mht.middle.webp";
+        String url = "https://www.baidu.com";
         Manhua manhua = new Manhua();
 
         System.out.println("manhua = " + manhua.getStatus(url));
